c1907e74bea96ab81162e15d9068877f4251b59e
Farhana26 <farhanarashid0@gmail.com>
2020-03-23 18:15:32 -0400

    initial commit

---
+++ amazon-prime/src/test/java/com/pru/amazon/MouseHoverAccount.java
@@ -0,0 +1,44 @@
+package com.pru.amazon;
+
+import com.pru.core.AbstractTestSetup;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class MouseHoverAccount extends AbstractTestSetup {
+
+    @Test
+    public void mousehover() throws InterruptedException {
+        WebElement searchBtn = driver.findElement(By.id("nav-link-accountList"));
+
+        Actions actions = new Actions(driver);
+      //action.moveToElement(searchBtn).click().build().perform();
+        actions.moveToElement(searchBtn);
+
+       // action.moveToElement(mainMenu).moveToElement(driver.findElement(By.xpath("submenuxpath"))).click().build().perform();
+
+        WebElement subMenu = driver.findElement(By.xpath("//span[@class=\"nav-text\"][text()='Your Account']"));
+        actions.moveToElement(subMenu);
+        actions.click().build().perform();
+        Thread.sleep(2000);
+        System.out.println("clicked");
+    }
+
+   /*public void mousehover(){
+        Actions actions = new Actions(driver);
+        WebElement mainMenu = driver.findElement(By.linkText("menulink"));
+        actions.moveToElement(mainMenu);
+
+        WebElement subMenu = driver.findElement(By.cssSelector("subLinklocator"));
+        actions.moveToElement(subMenu);
+        actions.click().build().perform();
+
+    }*/
+
+    @Override
+    protected String getUrl() {
+        return "https://www.amazon.com";
+    }
+
+}
---
+++ amazon-prime/src/test/java/com/pru/core/AbstractTestSetup.java
@@ -0,0 +1,69 @@
+package com.pru.core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractTestSetup {
+
+  //  public static final String CROME_DRIVER_PATH = "C:\\Users\\farhana rashid\\IdeaProjects\\AmazonPrime\\Amazon\\Driver\\chromedriver.exe";
+  private static final String CHROME_DRIVER_PATH = "C:\\Users\\farhana rashid\\IdeaProjects\\Driver\\chromedriver.exe";
+  protected WebDriver driver;
+
+  @Parameters({"browserName"})
+  @BeforeMethod
+  public void setup(@Optional("chrome") final String browserName) throws InterruptedException {
+    initializeLocalDriver(browserName);
+    driver.get(getUrl());
+    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    driver.manage().window().maximize();
+    /*login();*/
+  }
+/*
+  public void login() throws InterruptedException {
+    WebElement searchBtn = driver.findElement(By.id("nav-link-accountList"));
+
+    Actions action = new Actions(driver);
+    action.moveToElement(searchBtn).click().build().perform();
+    Thread.sleep(2000);
+    driver.findElement(By.id("ap_email")).sendKeys("aboni.heaven@gmail.com");
+    driver.findElement(By.id("continue")).click();
+    driver.findElement(By.id("ap_password")).sendKeys("aroni156");
+    driver.findElement(By.id("signInSubmit")).click();
+  }*/
+
+  abstract protected String getUrl();
+
+  private void initializeLocalDriver(final String browserName) {
+    if (browserName.equalsIgnoreCase("chrome")) {
+      System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+      ChromeOptions chromeOptions = new ChromeOptions();
+      driver = new ChromeDriver(chromeOptions);
+    } else if (browserName.equalsIgnoreCase("firefox")) {
+      driver = new FirefoxDriver();
+    }
+  }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
+
+  public String selectOptionByVisibleText(WebElement element, String value) {
+    Select select = new Select(element);
+    select.selectByVisibleText(value);
+    return value;
+  }
+
+  @AfterMethod
+  public void close() {
+    driver.quit();
+  }
+}
